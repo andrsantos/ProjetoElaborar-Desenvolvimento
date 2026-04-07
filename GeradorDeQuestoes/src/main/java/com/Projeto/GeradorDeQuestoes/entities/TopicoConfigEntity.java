180d5e84214e244e1fb784f4e5fc9072f718e86d
AndreySantos <santosandrey146@gmail.com>
2026-04-07 10:08:12 -0300

    Atualizando entidade - 07/04/2026

--- GeradorDeQuestoes/src/main/java/com/Projeto/GeradorDeQuestoes/entities/TopicoConfigEntity.java
+++ GeradorDeQuestoes/src/main/java/com/Projeto/GeradorDeQuestoes/entities/TopicoConfigEntity.java
@@ -1,5 +1,7 @@
 package com.Projeto.GeradorDeQuestoes.entities;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -22,8 +24,19 @@ public class TopicoConfigEntity {
 
     @Column(name = "instrucoes_especificas", columnDefinition = "TEXT")
     private String instrucoesEspecificas;
+
+    @Column(name = "data_atualizacao")
+    private LocalDateTime dataAtualizacao;
     
 
+
+    public LocalDateTime getDataAtualizacao() {
+        return this.dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(LocalDateTime dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
 
     public String getId() {
         return this.id;
